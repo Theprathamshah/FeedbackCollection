3a7d906082c864d5398f7d24f0b5514c1954e06e
Pratham Shah <itspratham2911@gmail.com>
2025-11-03 16:52:45 +0530

    feat: :sparkles: added rds in private subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,18 @@
 provider "aws" {
-  region  = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket         = "terraform-mentoring-bucket"
-  # force_destroy  = true
-  # tags = {
-  #   Name = "Pratham_mentorship"
-  # }
-    lifecycle {
+  bucket = "terraform-mentoring-bucket"
+
+  lifecycle {
     prevent_destroy = true
     ignore_changes  = all
   }
 }
+
 resource "aws_s3_bucket_ownership_controls" "bucket_owner" {
   bucket = aws_s3_bucket.my_bucket.id
-
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
@@ -23,7 +20,6 @@ resource "aws_s3_bucket_ownership_controls" "bucket_owner" {
 
 resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.my_bucket.id
-
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
@@ -33,13 +29,8 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = aws_s3_bucket.my_bucket.id
 
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "404.html"
-  }
+  index_document { suffix = "index.html" }
+  error_document { key = "404.html" }
 
   routing_rule {
     condition {
@@ -54,7 +45,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
 
 resource "aws_s3_bucket_cors_configuration" "cors" {
   bucket = aws_s3_bucket.my_bucket.id
-
   cors_rule {
     allowed_headers = ["*"]
     allowed_methods = ["GET", "HEAD"]
@@ -130,36 +120,186 @@ resource "aws_s3_object" "static_files" {
 output "website_url" {
   value = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
 }
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+
+terraform {
+  backend "s3" {
+    bucket         = "mentorship-tf-state-bucket"
+    key            = "feedback-app/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
   }
 }
 
-# Security Group for EC2
-resource "aws_security_group" "ec2_sg" {
-  name        = "ec2-sg"
-  description = "Allow SSH, HTTP and HTTPS"
-  vpc_id      = data.aws_vpc.default.id
+########################################################
+# RDS Setup
+########################################################
+# data "aws_vpc" "default" {
+#   default = true
+# }
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+# data "aws_subnets" "default" {
+#   filter {
+#     name   = "vpc-id"
+#     values = [data.aws_vpc.default.id]
+#   }
+
+# }
+
+# # Generate password
+# resource "random_password" "db_password" {
+#   length  = 20
+#   special = true
+# }
+
+# # Store in Secrets Manager
+# resource "aws_secretsmanager_secret" "db_creds" {
+#   name        = "feedback-app/db-credentials"
+#   description = "DB credentials for feedback app RDS"
+# }
+
+# resource "aws_secretsmanager_secret_version" "db_credentials" {
+#   secret_id     = aws_secretsmanager_secret.db_creds.id
+#   secret_string = jsonencode({
+#     username = "feedback_admin"
+#     password = "SuperSecret123!"
+#     dbname   = "feedbackdb"
+#     host     = aws_db_instance.postgres.address
+#     port     = aws_db_instance.postgres.port
+#   })
+# }
+
+# # Subnet group for RDS
+# resource "aws_db_subnet_group" "pg_subnet" {
+#   name       = "feedback-db-subnet-group"
+#   subnet_ids = data.aws_subnets.default.ids
+# }
+
+# # Security group for RDS
+# resource "aws_security_group" "rds_sg" {
+#   name   = "feedback-rds-sg"
+#   vpc_id = data.aws_vpc.default.id
+
+#   ingress {
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"] # ⚠️ For dev only — restrict in prod
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+
+# # PostgreSQL DB
+# resource "aws_db_instance" "postgres" {
+#   identifier              = "feedback-postgres-db"
+#   allocated_storage       = 20
+#   engine                  = "postgres"
+#   engine_version          = "16.4"
+#   instance_class          = "db.t4g.micro"
+#   db_name                 = "feedbackdb"
+#   username                = "feedback_admin"
+#   password                = random_password.db_password.result
+#   port                    = 5432
+#   publicly_accessible     = true
+#   skip_final_snapshot     = true
+#   db_subnet_group_name    = aws_db_subnet_group.pg_subnet.name
+#   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+
+#   backup_retention_period = 7
+#   deletion_protection     = false
+# }
+
+# ########################################################
+# # Outputs
+# ########################################################
+# output "rds_endpoint" {
+#   value = aws_db_instance.postgres.endpoint
+# }
+
+# output "db_password" {
+#   sensitive = true
+#   value     = random_password.db_password.result
+  
+# }
+# output "rds_url" {
+#   sensitive = true
+#   value = "postgresql://feedback_admin:${random_password.db_password.result}@${aws_db_instance.postgres.address}:${aws_db_instance.postgres.port}/feedbackdb"
+# }
+
+# output "db_credentials_secret_arn" {
+#   value = aws_secretsmanager_secret.db_creds.arn
+# }
+
+
+resource "aws_vpc" "feedback_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "feedback-vpc"
   }
+}
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.feedback_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+  tags = { Name = "feedback-public-subnet" }
+}
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.feedback_vpc.id
+  cidr_block        = "10.0.10.0/24"   # changed to avoid conflict
+  availability_zone = "ap-south-1a"
+  tags = {
+    Name = "private-subnet-1a"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.feedback_vpc.id
+  cidr_block        = "10.0.11.0/24"   # changed to avoid conflict
+  availability_zone = "ap-south-1b"
+  tags = {
+    Name = "private-subnet-1b"
+  }
+}
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.feedback_vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.feedback_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_security_group" "rds_sg" {
+  name   = "feedback-rds-sg"
+  vpc_id = aws_vpc.feedback_vpc.id
 
   ingress {
-    description = "HTTP"
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["106.201.147.144/32"]
   }
 
   egress {
@@ -168,76 +308,52 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  tags = { Name = "feedback-rds-sg" }
+}
+resource "aws_db_subnet_group" "pg_subnet" {
+  name       = "feedback-db-subnet-group2"
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id
+  ]
+  tags = { Name = "feedback-db-subnet-group2" }
 }
 
-# Key Pair
-variable "public_key" {
-  description = "Public key for EC2 SSH access"
-  type        = string
+# PostgreSQL DB
+resource "aws_db_instance" "postgres" {
+  identifier              = "feedback-postgres-db"
+  allocated_storage       = 20
+  engine                  = "postgres"
+  engine_version          = "16.10"
+  instance_class          = "db.t4g.micro"
+  db_name                 = "feedbackdb"
+  username                = "feedback_admin"
+  password                = "SuperSecret123"
+  port                    = 5432
+
+  db_subnet_group_name    = aws_db_subnet_group.pg_subnet.name
+  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
+
+  publicly_accessible     = true
+  skip_final_snapshot     = true
+
+  backup_retention_period = 7
+  deletion_protection     = false
+
+
+  tags = { Name = "feedback-postgres-db" }
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "mentorship-key"
-  public_key = var.public_key
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.endpoint
 }
 
-# Fetch default VPC
-data "aws_vpc" "default" {
-  default = true
+output "rds_url" {
+  sensitive = true
+  value     = "postgresql://feedback_admin:SuperSecret123@${aws_db_instance.postgres.address}:${aws_db_instance.postgres.port}/feedbackdb"
 }
 
-# Fetch default subnets
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-# EC2 Instance
-resource "aws_instance" "my_ec2" {
-  ami                    = data.aws_ami.amazon_linux.id
-  instance_type          = "t3.micro"
-  subnet_id              = element(data.aws_subnets.default.ids, 0)
-  key_name               = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
-  tags = {
-    Name = "FeedbackCollection-Backend-EC2"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              # Install Node.js 18.x (Amazon Linux 2)
-              curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
-              yum install -y nodejs git
-
-              # Install PM2 (to keep Node app running)
-              npm install -g pm2
-
-              # Create app directory
-              mkdir -p /home/ec2-user/app
-              cd /home/ec2-user/app
-
-              # For simplicity, clone from GitHub (replace with your repo URL)
-              git clone https://github.com/Theprathamshah/FeedbackCollection.git
-              cd FeedbackCollection/backend
-
-              # Install dependencies
-              npm install
-
-              # Start the backend (assuming index.js or app.js)
-              pm2 start index.ts --name feedback-backend
-              pm2 startup systemd
-              pm2 save
-              EOF
-}
-
-output "ec2_public_ip" {
-  value = aws_instance.my_ec2.public_ip
-}
-
-output "ec2_public_dns" {
-  value = aws_instance.my_ec2.public_dns
+output "db_password" {
+  sensitive = true
+  value     = "SuperSecret123"
 }
